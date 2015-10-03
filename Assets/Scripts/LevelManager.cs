34bcbd414a37ecde516e3b8dacc9c8833a2d0e13
DnLKnR <konia013@umn.edu>
2015-10-03 14:22:09 -0500

    Particle Effects (Death and Respawn Animations)

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -7,6 +7,11 @@ public class LevelManager : MonoBehaviour {
 
 	private PlayerController player;
 
+	public GameObject deathParticle;
+	public GameObject respawnParticle;
+
+	public float respawnDelay;
+
 	// Use this for initialization
 	void Start () {
 		//Get PlayerController Object that already exists in the scene
@@ -19,8 +24,29 @@ public class LevelManager : MonoBehaviour {
 	}
 
 	public void RespawnPlayer(){
-		Debug.Log ("Player Respawn");
-		//Reset the players position to their current checkpoint position
+		//Start the death and respawn as a co-routine
+		StartCoroutine("RespawnPlayerCo");
+	}
+
+	public IEnumerator RespawnPlayerCo(){
+		Instantiate(deathParticle, player.transform.position, player.transform.rotation);
+		//Turn player off
+		TogglePlayer(false);
+		//Timer set up to wait before respawning the player
+		yield return new WaitForSeconds(respawnDelay);
+		//Reset the players position to their last checkpoint position
 		player.transform.position = currentCheckpoint.transform.position;
+		Instantiate(respawnParticle, currentCheckpoint.transform.position, currentCheckpoint.transform.rotation);
+		Debug.Log ("Player Respawn");
+		//Turn player back on
+		TogglePlayer(true);
+	}
+
+	public void TogglePlayer(bool isEnabled){
+		Renderer renderer = player.GetComponent<Renderer>();
+		//Disable player being controlled
+		player.enabled = isEnabled;
+		//Disable player form being visible
+		renderer.enabled = isEnabled;
 	}
 }
